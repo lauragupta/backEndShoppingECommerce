8bd6174e1ad361993616825cd5df959bac07cfb3
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-06 11:50:17 -0800

    add relationships on index.js

--- models/index.js
+++ models/index.js
@@ -3,15 +3,36 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsTo } = require('./Product');
 
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  },
+  as:'product_tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  },
+  as:'tag_product'
+});
 
 module.exports = {
   Product,
