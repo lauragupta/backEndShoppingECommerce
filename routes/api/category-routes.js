4ed3e2c9bd4df9c1c7aa14e03bbcc8d20672369f
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-06 17:50:04 -0800

    add first category get route successfully

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,15 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', async (req, res) => {
