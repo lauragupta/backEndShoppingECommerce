de5f3f5ebce22f9c633900abad36b033c7e370d1
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-06 23:22:17 -0800

    add get and delete routes for product including testing and add get root route for tag and all async for tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,14 +36,38 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  console.log("hit");
+  const categoryData = await Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+  return res.json(categoryData);
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const categoryData = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  return res.json(categoryData);
 });
 
 module.exports = router;
